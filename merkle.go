c300b096ff2efd0c2f59beaf10e675e1c7bad4ca
Humayun Javed <humayunjaved23@gmail.com>
2023-02-03 02:19:28 +0500

    retunring

--- merkle.go
+++ merkle.go
@@ -60,7 +60,6 @@ func GenerateMerkleRoot(leaves [][]byte) []byte {
 
 func _generateLevel(hashes [][]byte, tree [][][]byte) [][][]byte {
 	if len(hashes) == 1 {
-		tree = append(tree, hashes)
 		return tree
 	}
 	hashes = ensureEven(hashes)
